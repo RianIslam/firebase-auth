977574b32870d392131e62092df9e9fc9fd65815
RianIslam <rianislamdr@gmail.com>
2021-03-13 23:23:00 +0600

    auth firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
 
+import "firebase/auth";
+
+import firebaseConfig from './fireBaseConfig';
+import { useState } from 'react';
+firebase.initializeApp(firebaseConfig)
 function App() {
+
+  const [user,setUser] = useState({
+    isSignIn:false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  const handleSignIn = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res =>{
+      const {displayName,photoURL,email} = res.user;
+      const signInUser ={
+        isSignIn:true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+      setUser(signInUser)
+      console.log(displayName,photoURL,email)
+      console.log(res)
+    })
+    .catch(err =>{
+      console.log(err);
+      console.log(err.message);
+    })
+  }
+  const handleSingOut = () =>{
+    firebase.auth().signOut()
+    .then(res =>{
+      const signedOutUser = {
+        isSignIn:false,
+        name:'',
+        photo:'',
+        email: ''
+      }
+      setUser(signedOutUser)
+    })
+    .catch(err =>{
+
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      user.isSignIn ? <button onClick={handleSingOut}>Sign Out</button>:
+      <button onClick={handleSignIn}>Sign In</button>
+    }
+      
+      {
+        user.isSignIn && <div>
+         <p>welcome{user.name}</p>
+          <p>Your email : {user.email}</p>
+          <img src={user.photo} alt=""/>
+      </div>
+      }
     </div>
   );
 }
